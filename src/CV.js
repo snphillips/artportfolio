82df64dab9adda1b555ac85034becb3a24e86a92
Sarah Phillips <snphillips@gmail.com>
2020-03-15 07:58:37 -0400

    first commit

---
+++ src/CV.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './index.css';
+
+
+
+function CV() {
+  return (
+    <div className="cv">(insert cv)</div>
+  );
+}
+
+export default CV;
---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './index.css';
+
+
+
+function Header() {
+  return (
+    <header className="header">Sarah Nicole Phillips</header>
+  );
+}
+
+export default Header;
